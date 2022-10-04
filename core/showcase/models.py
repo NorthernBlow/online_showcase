dd669af6aa134c4ba7bdccad3a01384d46afa2ed
NorthernBlow <rheinhart@yandex.ru>
2022-10-04 19:51:21 +0300

    create models

--- core/showcase/models.py
+++ core/showcase/models.py
@@ -4,4 +4,15 @@ class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
 
+
+    class Meta:
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+
 # Create your models here.
