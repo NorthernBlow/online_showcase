875b4ad8882e877e626daee0980e022572eaaac3
NorthernBlow <rheinhart@yandex.ru>
2022-10-05 23:31:17 +0300

    create views, models

--- core/showcase/views.py
+++ core/showcase/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    print(request)
+    print('Hello')
+    return HttpResponse('Landing')
+
+def test(request):
+    return HttpResponse('fuck')
