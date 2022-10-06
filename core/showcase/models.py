f1978e5e419e0f4e4af2cceddb83f3143c0b8498
NorthernBlow <rheinhart@yandex.ru>
2022-10-06 19:28:09 +0300

    создал админку, зарегистрировал категории и товары

--- core/showcase/models.py
+++ core/showcase/models.py
@@ -4,13 +4,14 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
-    photo = models.ImageField(upload_to='content/images/%Y%m%d/')
+    photo = models.ImageField(upload_to='content/images/%Y%m%d/', blank=True)
     is_published = models.BooleanField(default=True)
 
 
     class Meta:
         verbose_name_plural = 'categories'
 
+    #вместо QuerySet названия объекта используем метод для строкового представления объекта
     def __str__(self):
         return self.name
 
@@ -21,7 +22,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='content/product_images/%Y%m%d/')
+    image = models.ImageField(upload_to='content/product_images/%Y%m%d/', blank=True)
     slug = models.SlugField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     in_stock = models.BooleanField(default=True)
@@ -34,7 +35,6 @@ class Product(models.Model):
         verbose_name_plural = 'Products'
         ordering = ('-created_at', )
 
-
+    #вместо QuerySet названия объекта используем метод для строкового представления объекта
     def __str__(self):
         return self.title
-# Create your models here.
--- core/showcase/views.py
+++ core/showcase/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Product
 
 # Create your views here.
 def index(request):
