6985b6b302ee99b8b9d2290a90f7a9d0d82d62f7
NorthernBlow <rheinhart@yandex.ru>
2022-10-06 05:02:15 +0300

    fully create models :D

--- core/showcase/models.py
+++ core/showcase/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
+    photo = models.ImageField(upload_to='content/images/%Y%m%d/')
+    is_published = models.BooleanField(default=True)
 
 
     class Meta:
-        verbose_name_plural = 'Категории'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
@@ -14,5 +17,24 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default='admin')
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='content/product_images/%Y%m%d/')
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+
+    class Meta:
+        verbose_name_plural = 'Products'
+        ordering = ('-created_at', )
+
+
+    def __str__(self):
+        return self.title
 # Create your models here.
