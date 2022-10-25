479c4103ed747ca06f87c6506217428d6a19efc8
NorthernBlow <rheinhart@yandex.ru>
2022-10-25 07:02:26 +0300

    Покрыл то, что накодил тем, что называется тестами

--- core/showcase/tests/test_models.py
+++ core/showcase/tests/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from showcase.models import Category, Product
+from django.contrib.auth.models import User
 
 
 class TestCategoriesModel(TestCase):
@@ -14,3 +15,28 @@ class TestCategoriesModel(TestCase):
         """
         data = self.data1
         self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_name(self):
+        """
+        Test Category model return data name
+        :return:
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(category_id=1, title="django", slug='django', created_by_id=1,
+                                            price='2000', image='', is_active=1, in_stock=1, id=1)
+
+    def test_product_model_entry(self):
+        """
+        Test Product model data
+        :return:
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+        self.assertEqual(str(data), 'django')
